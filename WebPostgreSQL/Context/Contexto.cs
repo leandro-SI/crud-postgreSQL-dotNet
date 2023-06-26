c9cda2b1aa14d6c514c5b7d5de8e31b11096be6b
Leandro <leandro.almeida@uvvnet.com.br>
2023-06-25 22:46:18 -0300

    implementando contexto e conexão com o postgre

---
+++ WebPostgreSQL/Context/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebPostgreSQL.Models;
+
+namespace WebPostgreSQL.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        DbSet<Produto> Produtos { get; set; }
+    }
+}
--- WebPostgreSQL/Program.cs
+++ WebPostgreSQL/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebPostgreSQL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<Contexto>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionPostgre")));
 
 var app = builder.Build();
 
